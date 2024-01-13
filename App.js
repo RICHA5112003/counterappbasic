51bea5c34cc70fb7f26fdc32c682f0ce11a736b4
RICHA5112003 <137594756+RICHA5112003@users.noreply.github.com>
2024-01-13 19:02:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Cc from './cc';
+function App() {
+  return (
+    <div className="App">
+      <div className='fL'><Cc/></div>
+      <div className='fR'><Cc/></div>
+    </div>
+  );
+}
+
+export default App;
